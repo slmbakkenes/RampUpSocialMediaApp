946c7e9dedd62bec0d261223f2be91809522cc68
slmbakkenes <slmbakkenes@gmail.com>
2024-09-18 14:02:22 +0200

    Added migrations

--- ramp_up_project/blog/models.py
+++ ramp_up_project/blog/models.py
@@ -1,23 +1,55 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+import uuid
+from datetime import datetime
 
+User = get_user_model()
 
-STATUS = (
-    (0,"Draft"),
-    (1,"Publish")
-)
-
-class Post(models.Model):
-    title = models.CharField(max_length=200, unique=True)
-    slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_posts')
-    updated_on = models.DateTimeField(auto_now= True)
-    content = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
-    status = models.IntegerField(choices=STATUS, default=0)
-
-    class Meta:
-        ordering = ['-created_on']
+# Create your models here
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    id_user = models.IntegerField()
+    bio = models.TextField(blank=True)
+    profile_img = models.ImageField(upload_to='profile_images', default='blank-profile-picture.png')
 
     def __str__(self):
-        return self.title
+        return self.user.username
+
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    user = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='post_images')
+    caption = models.TextField()
+    created_at = models.DateTimeField(default=datetime.now)
+    no_of_likes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.user
+
+class LikePost(models.Model):
+    post_id = models.CharField(max_length=500)
+    user = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.user
+
+class FollowersCount(models.Model):
+    follower = models.CharField(max_length=100)
+    user = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.user
+
+class Comment(models.Model):
+    post_id = models.CharField(max_length=500)
+    user_id = models.CharField(max_length=500)
+    comment = models.TextField()
+    comment_date = models.DateTimeField(default=datetime.now)
+
+class Category(models.Model):
+    category_id = models.CharField(max_length=500)
+    category_name = models.CharField(max_length=100)
+
+class CategoryPost(models.Model):
+    category_id = models.CharField(max_length=500)
+    post_id = models.CharField(max_length=500)
