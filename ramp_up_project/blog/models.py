5bff6bc16d7475faf2316cd1ee0e38d2a464e122
Boaz Bakhuijzen <b.bakhuijzen@flooris.nl>
2024-09-20 10:11:32 +0200

    Quick fix for database foreign keys
    - Corrected foreign keys to actual keys

--- ramp_up_project/blog/models.py
+++ ramp_up_project/blog/models.py
@@ -8,16 +8,15 @@ User = get_user_model()
 # Create your models here
 class Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    id_user = models.IntegerField()
     bio = models.TextField(blank=True)
     profile_img = models.ImageField(upload_to='profile_images', default='blank-profile-picture.png')
 
     def __str__(self):
-        return self.user.username
+        return self.user
 
 class Post(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
-    user = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='post_images')
     caption = models.TextField()
     created_at = models.DateTimeField(default=datetime.now)
@@ -27,29 +26,37 @@ class Post(models.Model):
         return self.user
 
 class LikePost(models.Model):
-    post_id = models.CharField(max_length=500)
-    user = models.CharField(max_length=500)
+    post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.user
 
 class FollowersCount(models.Model):
     follower = models.CharField(max_length=100)
-    user = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.user
 
 class Comment(models.Model):
-    post_id = models.CharField(max_length=500)
-    user_id = models.CharField(max_length=500)
+    post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
     comment = models.TextField()
     comment_date = models.DateTimeField(default=datetime.now)
 
+    def __str__(self):
+        return self.user
+
 class Category(models.Model):
-    category_id = models.CharField(max_length=500)
     category_name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.category_name
+
 class CategoryPost(models.Model):
-    category_id = models.CharField(max_length=500)
-    post_id = models.CharField(max_length=500)
+    category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
+    post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.category_id
