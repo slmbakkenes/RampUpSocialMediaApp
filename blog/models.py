66148e9caf829d4f601ebd1fd084b6027594c7ae
slmbakkenes <slmbakkenes@gmail.com>
2024-09-30 22:16:50 +0200

    folder structure update

---
+++ blog/models.py
@@ -0,0 +1,62 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+import uuid
+from datetime import datetime
+
+User = get_user_model()
+
+# Create your models here
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    profile_img = models.ImageField(upload_to='profile_images', default='blank-profile-picture.png')
+
+    def __str__(self):
+        return self.user.username
+
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='post_images', blank=True)
+    caption = models.TextField()
+    created_at = models.DateTimeField(default=datetime.now)
+    no_of_likes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return str(self.id)
+
+class LikePost(models.Model):
+    post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+
+class FollowersCount(models.Model):
+    follower = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+
+class Comment(models.Model):
+    post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+    comment = models.TextField()
+    comment_date = models.DateTimeField(default=datetime.now)
+
+    def __str__(self):
+        return str(self.id)
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.category_name
+
+class CategoryPost(models.Model):
+    category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
+    post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.category_id.category_name
---
+++ blog/views.py
@@ -0,0 +1,55 @@
+from msilib.schema import ListView
+
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DetailView
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+
+from blog.models import Post
+from forms.post_form import PostForm
+
+# Create your views here.
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "registration/signup.html"
+
+class PostCreationView(CreateView):
+    form_class = UserCreationForm
+    template_name = "Post/create_post.html"
+
+    @login_required
+    def create_post_view(request):
+        if request.method == 'POST':
+            form = PostForm(request.POST, request.FILES)
+            if form.is_valid():
+                post = form.save(commit=False)
+                post.user = request.user  # Koppel de post aan de ingelogde gebruiker
+                post.save()
+                return redirect('profile', user_id=request.user.id)  # Redirect naar de profielpagina
+        else:
+            form = PostForm()
+        return render(request, 'create_post.html', {'form': form})
+
+def list_posts_view(request):
+    posts = Post.objects.all()  # Haal alle posts op
+    return render(request, 'list_posts.html', {'posts': posts})
+
+def update_post_view(request, post_id):
+    post = get_object_or_404(Post, id=post_id)  # Haal de post op
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES, instance=post)  # Geef het bestaande object door
+        if form.is_valid():
+            form.save()
+            return redirect('profile', user_id=request.user.id)  # Redirect naar de profielpagina
+    else:
+        form = PostForm(instance=post)  # Vul het formulier met de huidige gegevens van de post
+    return render(request, 'update_post.html', {'form': form, 'post': post})
+
+def delete_post_view(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    if request.method == 'POST':
+        post.delete()  # Verwijder de post
+        return redirect('profile', user_id=request.user.id)  # Redirect naar de profielpagina
+    return render(request, 'confirm_delete.html', {'post': post})
