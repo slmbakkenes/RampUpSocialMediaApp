4edac9e0f5a736ce6391577189748f67828a9171
CBOnyx <74596681+CBOnyx@users.noreply.github.com>
2024-09-30 08:54:15 +0200

    push

--- ramp_up_project/blog/views.py
+++ ramp_up_project/blog/views.py
@@ -5,6 +5,7 @@ from django.urls import reverse_lazy
 from django.views.generic import CreateView, DetailView
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+
 from forms.post_form import PostForm
 
 # Create your views here.
@@ -24,5 +25,20 @@ def create_post_view(request):
             return redirect('home')  # Redirect naar bijvoorbeeld de homepagina
     else:
         form = PostForm()
+
+    return render(request, 'create_post.html', {'form': form})
+
+
+def edit_post_view(request, post_id):
+
+    if request.method == 'PUT':
+        form = PostForm(request.PUT, request.FILES)  # Vul het formulier met de bestaande post
+        if form.is_valid():
+            post = form.save(commit=False)  # Sla het formulier op, maar nog niet in de database
+            post.user = request.user  # Vul de user ForeignKey met de ingelogde gebruiker
+            post.save()  # Sla nu de post op
+            return redirect('home')  # Redirect naar bijvoorbeeld de homepagina
+    else:
+        form = PostForm()  # Vul het formulier met de bestaande post
 
     return render(request, 'create_post.html', {'form': form})
