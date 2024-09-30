2324ab0d3b8dce46c0f5209d84191675fbde013b
Boaz Bakhuijzen <b.bakhuijzen@flooris.nl>
2024-09-30 09:22:46 +0200

    Add following system to posts
    - Changed foryoupage to show the posts and comments and added styling
    - changed urls.py to show the foryoupagetemplate also at index, so it shows all posts there
    - Added Cache to gitignore
    - Added package.json for dependencies
    - Changed views.py to query the correct set of posts

--- ramp_up_project/blog/views.py
+++ ramp_up_project/blog/views.py
@@ -1,8 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, ListView
-
-from blog.models import Post
+from blog.models import Post, Comment, Follow
 
 
 # Create your views here.
@@ -11,7 +10,23 @@ class SignUpView(CreateView):
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
 
+
 class ForYouPageView(ListView):
     template_name = "forYouPage.html"
-    paginate_by = 10
+    paginate_by = 15
     model = Post
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        path = self.request.path
+
+        if "/foryoupage/" == path:
+            following = Follow.objects.filter(follower=self.request.user).values_list('following', flat=True)
+            queryset = queryset.filter(user__in=following)
+
+        return queryset.order_by("-created_at")
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['comments'] = Comment.objects.all()
+        return context
