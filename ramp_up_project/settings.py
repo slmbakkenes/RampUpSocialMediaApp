8ebd874912908e3b50bc4d03e8f54f33e8154ebd
slmbakkenes <slmbakkenes@gmail.com>
2024-10-03 19:49:09 +0200

    added package to make admin charts.

--- ramp_up_project/settings.py
+++ ramp_up_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'admin_tools_stats',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
