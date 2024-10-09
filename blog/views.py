01538dfe201d08a07831b2099e4689d73a569a39
CBOnyx <74596681+CBOnyx@users.noreply.github.com>
2024-10-09 08:58:26 +0200

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -91,7 +91,7 @@ class PostUpdateView(LoginRequiredMixin, UpdateView):
 @login_required
 class PostDeleteView(LoginRequiredMixin, DeleteView):
     model = Post
-    template_name = 'confirm_delete.html'
+    template_name = 'profile/profile.html'
 
     def get_success_url(self):
         return reverse_lazy('profile', kwargs={'user_id': self.request.user.id})
