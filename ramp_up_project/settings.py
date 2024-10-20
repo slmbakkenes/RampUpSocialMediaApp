cb034d7ba9d68f0e14aadea3150cf98da12d624d
slmbakkenes <slmbakkenes@gmail.com>
2024-10-20 22:12:33 +0200

    updated settings.py

--- ramp_up_project/settings.py
+++ ramp_up_project/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
