e3e69b84086c00c9a4f65e67d915a4c7a0d3bdb1
slmbakkenes <slmbakkenes@gmail.com>
2024-10-20 18:50:37 +0200

    debug on

--- ramp_up_project/settings.py
+++ ramp_up_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6d@msb!qlqt38n2*ipbb0v3sh8y1%r(ac5o*xj-!+h=hjky1(_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['ramp-up-project.azurewebsites.net']
 
